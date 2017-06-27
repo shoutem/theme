c58c5602bae129d637e60f69f564750b81047400
Daniel Levitt <daniellevitt86@gmail.com>
2017-06-27 10:45:21 +0100

    Move index into src
    Also remove examples

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import connectStyle from './connectStyle';
+import { INCLUDE } from './resolveIncludes';
+import StyleProvider from './StyleProvider';
+import Theme, { ThemeShape } from './Theme';
+import {
+  createVariations,
+  createSharedStyle,
+  changeColorAlpha,
+  getSizeRelativeToReference,
+  inverseColorBrightnessForAmount,
+} from './addons';
+
+export {
+  connectStyle,
+  INCLUDE,
+  StyleProvider,
+  Theme,
+  ThemeShape,
+  createVariations,
+  createSharedStyle,
+  changeColorAlpha,
+  getSizeRelativeToReference,
+  inverseColorBrightnessForAmount,
+};
